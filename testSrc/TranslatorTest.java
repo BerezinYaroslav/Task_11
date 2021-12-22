12b8e0454f5d496e6a7f89e9e7f49e4fc2fa39ec
yaroslavberezin <yb2003work@gmail.com>
2021-12-22 22:54:56 +0300

    V1.1

--- testSrc/TranslatorTest.java
+++ testSrc/TranslatorTest.java
@@ -11,71 +11,81 @@ public class TranslatorTest {
     String actualResult;
 
     @Test
-    public void test1() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/1.txt")));
+    public void translatorTest1() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/1.txt")));
         correctResult = "один";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test2() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/2.txt")));
+    public void translatorTest2() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/2.txt")));
         correctResult = "двенадцать";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test3() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/3.txt")));
+    public void translatorTest3() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/3.txt")));
         correctResult = "пятьдесят семь";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test4() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/4.txt")));
+    public void translatorTest4() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/4.txt")));
         correctResult = "сто двадцать один";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test5() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/5.txt")));
+    public void translatorTest5() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/5.txt")));
         correctResult = "шесть тысяч семьсот восемьдесят девять";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test6() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/6.txt")));
+    public void translatorTest6() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/6.txt")));
         correctResult = "сто двенадцать тысяч триста сорок пять";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test7() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/7.txt")));
+    public void translatorTest7() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/7.txt")));
         correctResult = "девятьсот девяносто девять тысяч девятьсот девяносто девять";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test8() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/8.txt")));
+    public void translatorTest8() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/8.txt")));
         correctResult = "один миллион";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test9() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/9.txt")));
+    public void translatorTest9() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/9.txt")));
         correctResult = "Введенное число должно быть от 1 до 1 000 000.";
         Assert.assertEquals(correctResult, actualResult);
     }
 
     @Test
-    public void test10() throws FileNotFoundException {
-        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile("testSrc/testFiles/10.txt")));
+    public void translatorTest10() throws FileNotFoundException {
+        actualResult = String.valueOf(translator.translateNumberToWords(translator.readIntFromFile
+                ("testSrc/testFiles/10.txt")));
         correctResult = "Введенное число должно быть от 1 до 1 000 000.";
         Assert.assertEquals(correctResult, actualResult);
     }
